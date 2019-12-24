608c5f06425d4c27561cf67e781f1f4b74183824
aerain <j40f893@gmail.com>
2019-12-24 11:35:02 +0900

    스크립트 변경

--- popup_script.js
+++ popup_script.js
@@ -1,3 +1,30 @@
 var adButton = document.getElementById('ad')
 var tierButton = document.getElementById('tier')
 
+function removeAd() {
+    // 현재 크롬 창중에 활성화된 탭에 다음 함수 실행.
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tabs => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { send: "removeAd" }
+        )
+    })
+}
+
+function manipulateTier() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tabs => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { send: "manipulateTier" }
+        )
+    })
+}
+
+adButton.addEventListener('click', removeAd);
+tierButton.addEventListener('click', manipulateTier);
--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    msg.send === 'removeAd' && removeAd();
+    msg.send === 'manipulateTier' && manipulateTier();
 
-window.onload = function () {
+    /* 이렇게도 쓰면됨
+    if(msg.send === 'removeAd') removeAd();
+    if(msg.send === 'manipulateTier') manipulateTier();
+    */
+})
+
+function manipulateTier() {
     var tierImage = document.querySelector('.SummonerRatingMedium .Image')
     var tierRank = document.querySelector('.TierRankInfo .TierRank')
-    var topad = document.querySelector('.vm-placement');
     tierImage.src = "//opgg-static.akamaized.net/images/medals/iron_1.png?image=q_auto&v=1"
     tierRank.textContent = "Iron 5";
+}
+function removeAd() {
+    var topad = document.querySelector('.vm-placement');
     topad.remove();
 }
